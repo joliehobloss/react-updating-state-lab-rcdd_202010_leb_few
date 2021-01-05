5e89e1baadd446470dd26d1af1dce70d0bf83d57
Jolie Hobloss <jolieshobloss@gmail.com>
2021-01-05 16:08:43 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,6 +1,7 @@
 // Code DigitalClicker Component Here
 import React from 'react';
 class DigitalClicker extends React.Component {
+<<<<<<< HEAD
    constructor() {
     super();
     
@@ -13,6 +14,16 @@ class DigitalClicker extends React.Component {
   handleClick = () => {
   this.setState({
     timesClicked:this.state.timesClicked + 1
+=======
+ 
+ this.state = {
+      timesClicked=0
+    };
+    
+  handleClick = () => {
+  this.setState({
+    timesClicked+=1
+>>>>>>> 71ace8bac1ad15910e6e23ea3ec7afc6dd62a901
   })
   };
     
@@ -20,13 +31,20 @@ class DigitalClicker extends React.Component {
     return (
     <div>
     <label for="">Times click</label>
+<<<<<<< HEAD
     <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+=======
+    <button onClick={this.handleClick}>click</button>
+>>>>>>> 71ace8bac1ad15910e6e23ea3ec7afc6dd62a901
     </div>
     );
   }
 }
+<<<<<<< HEAD
 export default DigitalClicker;
 
 
+=======
+>>>>>>> 71ace8bac1ad15910e6e23ea3ec7afc6dd62a901
 
 
